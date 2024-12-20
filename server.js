8d3e108a4f86ea3c28f3a4aa09ad490e9bba1e63
AvinashRaut1998 <avinashraut2498@gmail.com>
2024-12-20 15:20:12 +0530

    shift to online server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -28,3 +29,5 @@ app.listen("3000", () => {
 */
 
 // for testing purpose of git...
+
+// dRrEdOAsndO6lq8o
